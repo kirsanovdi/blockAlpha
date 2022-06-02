3995a37986bfef3c8b14ab1412e90e6e9cb58ab8
kirsanovdi <kirsanov.di@edu.spbstu.ru>
2022-06-02 08:58:19 +0300

    correct main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,19 @@
 import controller.RTController;
 import engine.EngineRuntime;
 import graphics.GraphicsDisplay;
+import org.joml.Vector3i;
 
 public class Main {
     public static void main(String[] args) {
-        //RTController runtimeController = new RTController();
-        //EngineRuntime runtime = new EngineRuntime(runtimeController, EngineRuntime.generateRndBlocks(100, 25));
-        //GraphicsDisplay graphicsDisplay = new GraphicsDisplay(runtimeController,1080, 720, "Setup");
+        RTController runtimeController = new RTController();
+
+        GraphicsDisplay graphicsDisplay = new GraphicsDisplay(runtimeController, 1920, 1080, "Setup");
+        runtimeController.hookGraphicsDisplay(graphicsDisplay);
+
+        EngineRuntime runtime = new EngineRuntime(runtimeController, EngineRuntime.generateBlockLayer(new Vector3i(0, 5, 0), 25));
+        runtimeController.hookEngineRuntime(runtime);
 
 
-        //graphicsDisplay.run();
+        runtimeController.run();
     }
 }
